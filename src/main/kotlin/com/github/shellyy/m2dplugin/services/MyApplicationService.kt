656da28737dd7815634ce17207dc26796835f252
GitHub Action <action@github.com>
2021-01-07 06:32:22 +0000

    Template cleanup

--- src/main/kotlin/com/github/shellyy/m2dplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/shellyy/m2dplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.shellyy.m2dplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.shellyy.m2dplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/shellyy/m2dplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/shellyy/m2dplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.shellyy.m2dplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.shellyy.m2dplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
